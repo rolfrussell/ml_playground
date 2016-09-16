50f1e89dad7b033385bb3394a9801b0b7f4dedce
Rolf Russell <rolf@tablexi.com>
2016-09-15 22:18:22 -0500

    download_and_prepare_data.py

--- udacity/download_and_prepare_data.py
+++ udacity/download_and_prepare_data.py
@@ -6,14 +6,10 @@ import numpy as np
 import os
 import sys
 import tarfile
-from IPython.display import display, Image
 from scipy import ndimage
 from sklearn.linear_model import LogisticRegression
 from six.moves.urllib.request import urlretrieve
 from six.moves import cPickle as pickle
-
-# Config the matlotlib backend as plotting inline in IPython
-%matplotlib inline
 
 
 
