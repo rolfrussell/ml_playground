a0dd03b113ace5c4f387b6c60cc71b9aa861c036
Rolf Russell <rolf@tablexi.com>
2016-09-16 21:41:53 -0500

    assignment3_regularization.py

--- udacity/assignment3_regularization.py
+++ udacity/assignment3_regularization.py
@@ -17,14 +17,14 @@ import urllib.request
 pickle_file = 'notMNIST.pickle'
 
 # with open(pickle_file, 'rb') as f:
-#   save = pickle.load(f)
-#   train_dataset = save['train_dataset']
-#   train_labels = save['train_labels']
-#   valid_dataset = save['valid_dataset']
-#   valid_labels = save['valid_labels']
-#   test_dataset = save['test_dataset']
-#   test_labels = save['test_labels']
-#   del save  # hint to help gc free up memory
+#   datasets = pickle.load(f)
+#   train_dataset = datasets['train_dataset']
+#   train_labels = datasets['train_labels']
+#   valid_dataset = datasets['valid_dataset']
+#   valid_labels = datasets['valid_labels']
+#   test_dataset = datasets['test_dataset']
+#   test_labels = datasets['test_labels']
+#   del datasets  # hint to help gc free up memory
 #   print('Training set', train_dataset.shape, train_labels.shape)
 #   print('Validation set', valid_dataset.shape, valid_labels.shape)
 #   print('Test set', test_dataset.shape, test_labels.shape)
