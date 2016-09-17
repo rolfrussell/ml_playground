ea718cbd96f7a897e95b2ccb1fd5ed6950a0269b
Rolf Russell <rolf@tablexi.com>
2016-09-16 21:31:41 -0500

    assignment3_regularization.py

--- udacity/assignment3_regularization.py
+++ udacity/assignment3_regularization.py
@@ -16,18 +16,31 @@ import random
 
 pickle_file = 'notMNIST.pickle'
 
-with open(pickle_file, 'rb') as f:
-  save = pickle.load(f)
-  train_dataset = save['train_dataset']
-  train_labels = save['train_labels']
-  valid_dataset = save['valid_dataset']
-  valid_labels = save['valid_labels']
-  test_dataset = save['test_dataset']
-  test_labels = save['test_labels']
-  del save  # hint to help gc free up memory
-  print('Training set', train_dataset.shape, train_labels.shape)
-  print('Validation set', valid_dataset.shape, valid_labels.shape)
-  print('Test set', test_dataset.shape, test_labels.shape)
+# with open(pickle_file, 'rb') as f:
+#   save = pickle.load(f)
+#   train_dataset = save['train_dataset']
+#   train_labels = save['train_labels']
+#   valid_dataset = save['valid_dataset']
+#   valid_labels = save['valid_labels']
+#   test_dataset = save['test_dataset']
+#   test_labels = save['test_labels']
+#   del save  # hint to help gc free up memory
+#   print('Training set', train_dataset.shape, train_labels.shape)
+#   print('Validation set', valid_dataset.shape, valid_labels.shape)
+#   print('Test set', test_dataset.shape, test_labels.shape)
+
+s3_pickle_url = "https://s3.amazonaws.com/ml-playground/" + pickle_file
+datasets = pickle.load(urllib2.urlopen(s3_pickle_url))
+train_dataset = save['train_dataset']
+train_labels = save['train_labels']
+valid_dataset = save['valid_dataset']
+valid_labels = save['valid_labels']
+test_dataset = save['test_dataset']
+test_labels = save['test_labels']
+del datasets  # hint to help gc free up memory
+print('Training set', train_dataset.shape, train_labels.shape)
+print('Validation set', valid_dataset.shape, valid_labels.shape)
+print('Test set', test_dataset.shape, test_labels.shape)
 
 
 
