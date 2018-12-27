4f15929f6459d28cbd210cce174b4cbd5693bd93
Rolf Russell <rolf@tablexi.com>
2018-12-27 13:48:34 -0600

    create conda scripts

--- setup/create.conda.ml-py3.7.sh
+++ setup/create.conda.ml-py3.7.sh
@@ -2,11 +2,14 @@
 
 # create ml environment in anaconda
 
-conda create -n ml-py3.6 python=3.6
-conda activate ml-py3.6
+conda create -n ml-py3.7 python=3.7
+conda activate ml-py3.7
 
 conda install numpy pandas scipy scikit-learn matplotlib
 conda install jupyter
 
 pip install tensorflow
+error:  tensorflow not supported on python 3.7 yet
+https://github.com/tensorflow/tensorflow/issues/17022
+
 pip install keras
